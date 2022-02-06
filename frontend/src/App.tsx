50e0c79efc0bacfc0a12ff081cbbf88c71169cbb
Tony Alameda <tony.a@Tonys-MBP.local>
2022-02-05 20:20:18 -0800

    finished alignment of todolist as well as added delete button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,6 +23,14 @@ const App: FunctionComponent = () => {
     setDeadline(0);
   };
 
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== taskNameToDelete;
+      })
+    );
+  };
+
   return (
     <div className="App">
       <div className="header">
@@ -45,8 +53,8 @@ const App: FunctionComponent = () => {
         <button onClick={addTask}>Add Task</button>
       </div>
       <div className="todoList">
-        {todoList.map(() => {
-          return <TodoTask />;
+        {todoList.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })}
       </div>
     </div>
